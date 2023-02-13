98188276c7bc7b78709ddc1ded297da2072b37ce
Fedois <fedefox14@gmail.com>
2023-02-13 13:09:26 +0100

    inizializzato header

--- src/components/componentsHeader/AppHeader.vue
+++ src/components/componentsHeader/AppHeader.vue
@@ -1,13 +1,38 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data () {
+        return {
+            phoneOne: '(001) 88451234',
+            phoneTwo: '88455438',
+            listNav: ['home', 'about', 'service', 'showcase', 'blog', 'contact'],
+            socials: [
+                {
+                    icon: 'fa-brands fa-twitter',
+                    name: 'twitter'
+                },
+                {
+                    icon: 'fa-brands fa-facebook-f',
+                    name: 'facebook'
+                },
+                {
+                    icon: 'fa-brands fa-pinterest-p',
+                    name: 'pinterest'
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-h
+    <header>
+         
+    </header>
 </template>
 
 <style lang="scss" scoped>
-
+.phone-number{
+    background-color: black;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons'
+
+
+/* add icons to the library */
+library.add(faTwitter, faFacebookF, faPinterestP)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
